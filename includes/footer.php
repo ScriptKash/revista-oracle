ca1b98964b45b074f3a4ecdfb27c7e1ec32c26ee
Fernando <29582347+FernandoKash@users.noreply.github.com>
2019-11-03 11:07:51 -0600

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light navbar-shadow">
-        <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2019<a class="text-bold-800 primary">ProyectoDB,</a>Todos los derechos reservados</span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i class="feather icon-heart pink"></i></span>
+        <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2019<a class="text-bold-800 primary">ProyectoDB,</a>Todos los derechos reservados</span><span class="float-md-right d-none d-md-block">by <i class="feather icon-users primary"></i> Raychel, Denilson, Emmanuel, Fernando</span>
             <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="feather icon-arrow-up"></i></button>
         </p>
     </footer>
